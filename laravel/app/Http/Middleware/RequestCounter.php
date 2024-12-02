20f6a9264c8e034a38434c797a5017ee900c4417
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-02 11:18:31 +0200

    laravel <> RequestCounter middleware updated

--- laravel/app/Http/Middleware/RequestCounter.php
+++ laravel/app/Http/Middleware/RequestCounter.php
@@ -5,16 +5,19 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
 
 class RequestCounter
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
+        if ($userId = $request->header('user_id')) {
+            $user = User::find($userId);
+            if ($user) {
+                $user->increment('requests_num');
+            }
+        }
+
         return $next($request);
     }
 }
