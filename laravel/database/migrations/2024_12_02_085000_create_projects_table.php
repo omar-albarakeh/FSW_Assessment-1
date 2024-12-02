1e93af00929fd6ed122fd311b75b5e05d5df9df3
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-02 10:55:17 +0200

    laravel <> migrations updated

--- laravel/database/migrations/2024_12_02_085000_create_projects_table.php
+++ laravel/database/migrations/2024_12_02_085000_create_projects_table.php
@@ -9,13 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('projects', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up()
+{
+    Schema::create('projects', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->text('description')->nullable();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
